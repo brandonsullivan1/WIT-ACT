a2ccbe9b4dec15786765359cd8371f264eda9695
sullivanb13atwit <55287471+sullivanb13atwit@users.noreply.github.com>
2023-06-09 11:18:48 -0400

    Header/Banner and Homepage files made and new icon

---
+++ wit-act-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const Header = () => {
+    return (
+        <div className="site-header" role="banner">
+            <img src="../images/wit_logo.png" alt="Wentworth Institute of Technology" width="277" height="50"/>
+        </div>
+    )
+}
--- wit-act-app/src/pages/Homepage.jsx
+++ wit-act-app/src/pages/Homepage.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect, useRef } from "react"
 import { useNavigate } from "react-router-dom";
+import { Header } from "../components/Header";
 
 export const Homepage = () => {
     return (
+        <>
+        <Header />
         <div className="auth-form-container">
-
         </div>
+        </>
     )
 }
